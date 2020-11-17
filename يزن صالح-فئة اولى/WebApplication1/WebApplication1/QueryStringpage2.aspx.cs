e1059a1e11bab41e7654fcc8a4ac74e5da578e2f
Yazan Saleh <yznsalh93@gmail.com>
2020-11-17 11:53:17 +0200

    State Management

---
+++ يزن صالح-فئة اولى/WebApplication1/WebApplication1/QueryStringpage2.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class QueryStringpage2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+          Label1.Text=(Request.QueryString.Get("Name") + " is " + Request.QueryString.Get("Age")+" is " + Request.QueryString.Get("City")+" is "+Request.QueryString.Get("Gender"));
+            Label1.ForeColor = System.Drawing.Color.Blue;
+        }
+    }
+}
---
+++ يزن صالح-فئة اولى/WebApplication1/WebApplication1/SessionPage1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SessionPage1.aspx.cs" Inherits="WebApplication1.SessionPage1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    ISBN :<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+        <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+    </div>
+    </form>
+</body>
+</html>
